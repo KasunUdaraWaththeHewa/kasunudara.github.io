51ed8c435a5bfaf754dc8eacfbd21415bdf62416
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-02 12:22:07 +0530

    created responsive  navbar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,69 @@
+import './NavBar.css'
+import React, { useEffect } from 'react';
+import $ from 'jquery';
+
+function NavBar(){
+    useEffect(() => {
+        // jQuery code for navigation menu
+        $(function() {
+          // Open and close nav
+          $('#navbar-toggle').click(function() {
+            $('nav ul').slideToggle();
+          });
+    
+          // Hamburger toggle
+          $('#navbar-toggle').on('click', function() {
+            $(this).toggleClass('active');
+          });
+    
+          // If a link has a dropdown, add sub menu toggle.
+          $('nav ul li a:not(:only-child)').click(function(e) {
+            $(this).siblings('.navbar-dropdown').slideToggle('slow');
+    
+            // Close dropdown when selecting another dropdown
+            $('.navbar-dropdown').not($(this).siblings()).hide('slow');
+            e.stopPropagation();
+          });
+    
+          // Click outside the dropdown will remove the dropdown class
+          $('html').click(function() {
+            $('.navbar-dropdown').hide();
+          });
+        });
+      }, []);
+    return(
+        <div>
+            <section class="navigation">
+                <div class="nav-container">
+                    <div class="brand">
+                    <a href="#!">Kasun Udara</a>
+                    </div>
+                    <nav>
+                    <div class="nav-mobile"><a id="navbar-toggle" href="#!"><span></span></a></div>
+                    <ul class="nav-list">
+                        <li>
+                        <a href="#!">Home</a>
+                        </li>
+                        <li>
+                        <a href="#!">Projects</a>
+                        </li>
+                        <li>
+                        <a href="#!">Achievements</a>
+                        </li>
+                        <li>
+                        <a href="#!">Blogs</a>
+                        </li>
+                        <li>
+                        <a href="#!">Services</a>
+                        </li>
+                        <li>
+                        <a href="#!">Contact</a>
+                        </li>
+                    </ul>
+                    </nav>
+                </div>
+            </section>
+        </div>
+    );
+}
+export default NavBar;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+import * as React from 'react';
+import NavBar from './Components/NavBar';
+
+function Home(){
+    return(
+        <div className='HomeDiv'>
+            <NavBar />
+        </div>
+    );
+
+}
+export default Home;
