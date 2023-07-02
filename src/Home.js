a9fd0804339f1904a7bcf5b313b5b9cb6f3f91ae
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-02 12:59:18 +0530

    new commit

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,28 @@
 import './Home.css'
 import * as React from 'react';
 import NavBar from './Components/NavBar';
+import myphoto from './media/my photo.png'
 
 function Home(){
     return(
         <div className='HomeDiv'>
             <NavBar />
+            <div className="firstDiv">
+                <div className='meDetailsWithPhoto'>
+                    <div className="divPhoto">
+                        <img src={myphoto} alt="" />
+                    </div>
+                    <div className="divDetails">
+
+                    </div>
+                </div>
+            </div>
+            <div className="secondDiv">
+
+            </div>
+            <div className="thirdDiv">
+
+            </div>
         </div>
     );
 
