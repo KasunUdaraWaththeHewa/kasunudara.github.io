4a5559599247d2a3e8a48d471569b2736f3627de
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-02 14:52:09 +0530

    new commit

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import './Home.css'
 import * as React from 'react';
 import NavBar from './Components/NavBar';
 import myphoto from './media/my photo.png'
+import Button from 'react-bootstrap/Button';
 
 function Home(){
     return(
@@ -13,7 +14,14 @@ function Home(){
                         <img src={myphoto} alt="" />
                     </div>
                     <div className="divDetails">
-
+                        <h4>Software Engineer</h4>
+                        <h3>Kasun Udara</h3>
+                        <p>Introducing Kasun Udara, a passionate and dedicated software engineer with a strong drive for innovation and problem-solving. With a solid foundation in software development and a keen eye for detail, Kasun strives to deliver exceptional solutions that seamlessly blend functionality and user experience.</p>
+                        <div className='buttonsDiv'>
+                            <div className="btnDownload">Download CV</div>
+                        </div>
+                        
+                        
                     </div>
                 </div>
             </div>
