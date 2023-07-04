05e35d31e59c3463ac1136a960329c01c280ee82
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2023-07-04 23:33:31 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from './Home';
 import Project from './Project';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
-        <Project />
+        <Home />
     </div>
   );
 }
